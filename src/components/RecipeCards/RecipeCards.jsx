5a3b5c8866c33eab1918b98b0e73eccf7c05606d
carosteguig <carosteguig@gmail.com>
2022-03-31 16:28:00 -0700

    minor change to spelling on faves page

--- src/components/RecipeCards/RecipeCards.jsx
+++ src/components/RecipeCards/RecipeCards.jsx
@@ -1,23 +1,23 @@
 // import 'RecipeCards.scss';
 
 export default function RecipeCards(props) {
-console.log(props);
-    return (
+  console.log(props);
+  return (
 
-        <div>
-        <ul>
-          {/* {props && props.map((dataRecipe) => ( */}
-            <li key={props.recipe.id}>
-              <div>
-              <img src={props.recipe.image} alt={props.recipe.title} />
-              </div>
-              <div>
-                <h3>{props.recipe.title}</h3>
-              </div>
-            </li>
-          {/* ))} */}
-        </ul>
-      </div> 
+    <div>
+      <ul>
+        {/* {props && props.map((dataRecipe) => ( */}
+        <li key={props.recipe.id}>
+          <div>
+            <img src={props.recipe.image} alt={props.recipe.title} />
+          </div>
+          <div>
+            <h3>{props.recipe.title}</h3>
+          </div>
+        </li>
+        {/* ))} */}
+      </ul>
+    </div>
 
-    );
+  );
 }
